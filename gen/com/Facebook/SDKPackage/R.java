72a135cb48c889b37328e50edf774112b0d18ef6
Aaron Brady <abrady@fb.com>
2013-07-10 09:28:50 -0700

    cp working

--- gen/com/Facebook/SDKPackage/R.java
+++ gen/com/Facebook/SDKPackage/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.Facebook.SDKPackage;
+package com.facebook.SDKPackage;
 
 public final class R {
     public static final class attr {
@@ -14,6 +14,7 @@ public final class R {
         public static final int app_icon=0x7f020000;
     }
     public static final class string {
+        public static final int app_id=0x7f030001;
         public static final int app_name=0x7f030000;
     }
 }
